861c17bfe1e850cf2aa8e57ebf821788eeecd1ec
Rajan Makwana <rajan.b.makwana@gmail.com>
2024-12-05 20:26:07 +0530

    Implemented unit test case to handle empty string scenario and implemented solution for that

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export const add = function (numbers: string) {
+  // If string is empty then return 0
+  if (!numbers) return 0;
+
+  // extract numbers by spliting string
   const splitedString = numbers.split(/,/);
+
+  // Perform addition operation from splited data and return answer
   return splitedString.reduce((prev, curr) => {
     return prev + parseInt(curr);
   }, 0);
--- src/test/string-cal.test.ts
+++ src/test/string-cal.test.ts
@@ -2,7 +2,12 @@ import { add } from "../index";
 
 describe("String calculator unit test cases", () => {
   test("Should return sum of two numbers which is seperated by commas", () => {
-    const intStr = "2,5";
+    const intStr = "5,2";
     expect(add(intStr)).toBe(7);
+  });
+
+  test("Should return 0 if string is empty", () => {
+    const intStr = "";
+    expect(add(intStr)).toBe(0);
   });
 });
